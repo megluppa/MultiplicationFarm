8a688c76454533310fdf27738295e5864fefac06
Magdalena Kulawik-Luppa <magdalenakulawik@icloud.com>
2022-12-30 22:12:27 +0100

    Initial Commit

---
+++ MultiplicationFarm/MultiplicationFarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationFarmApp.swift
+//  MultiplicationFarm
+//
+//  Created by Magdalena Kulawik-Luppa on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationFarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
